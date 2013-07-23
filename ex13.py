ff3612056700d9c50dce195c041b7fc40949665b
Blake Levy <believey@gmail.com>
2013-07-23 02:36:16 -0700

    finished exercises 13 and 14

---
+++ ex13.py
@@ -0,0 +1,10 @@
+from sys import argv
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variables is:", second
+print "Your third variable is:", third
+print "Your fourth variables is:"
+fourth = raw_input("What would you like your fourth variable to be? ")
+print fourth
